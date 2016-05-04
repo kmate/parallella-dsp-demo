f588bbf75279c67f8138e5fdd55193eb31cd87ae
Karácsony Máté <k_mate@inf.elte.hu>
2016-05-04 18:53:42 +0200

    Valgrind measurements support

--- make_run.sh
+++ make_run.sh
@@ -5,8 +5,12 @@ dist/build/parallella-dsp-demo/parallella-dsp-demo
 gcc -std=c99 -Iinclude csrc/mongoose.c csrc/server.c -o dsp-server
 gcc -std=c99 -Iinclude -I../imperative-edsl/include ../imperative-edsl/csrc/chan.c gen/processor.c -lpthread -lm -o dsp-processor
 ./dsp-processor &
+# valgrind --tool=callgrind ./dsp-processor &
+# valgrind --tool=drd --show-stack-usage=yes ./dsp-processor 2>stack_usage.txt &
 ./dsp-server
+sleep 1
+# grep 'finished and used' stack_usage.txt
+# rm -rf stack_usage.txt
 rm -rf dsp-processor
 rm -rf dsp-server
 rm -rf *.fifo
-
