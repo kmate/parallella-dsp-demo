1186fc5ddb9b2e2195f9dccbbdd21e22782a01ef
Karácsony Máté <k_mate@inf.elte.hu>
2016-05-05 02:09:28 +0200

    Sorting thread usage statistics

--- make_run.sh
+++ make_run.sh
@@ -9,7 +9,7 @@ gcc -std=c99 -Iinclude -I../imperative-edsl/include ../imperative-edsl/csrc/chan
 # valgrind --tool=drd --show-stack-usage=yes ./dsp-processor 2>stack_usage.txt &
 ./dsp-server
 sleep 1
-# grep 'finished and used' stack_usage.txt
+# grep 'finished and used' stack_usage.txt | sort
 # rm -rf stack_usage.txt
 rm -rf dsp-processor
 rm -rf dsp-server
