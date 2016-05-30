16418dcb04db8a91749a5ea0e1394abcc3d42b61
Karácsony Máté <k_mate@inf.elte.hu>
2016-05-30 20:36:47 +0200

    Works on Epiphany again

--- make_run_epiphany.sh
+++ make_run_epiphany.sh
@@ -38,7 +38,7 @@ do
     SREC=../${f%.*}.srec
 
     # Build DEVICE side programs
-    e-gcc -std=gnu99 -ffast-math -fsingle-precision-constant -I../include -T ${ELDF} ../csrc/feldspar-parallella.c $f -o ${ELF} -le-lib -lm -Os
+    e-gcc -std=gnu99 -ffast-math -fsingle-precision-constant -I../include -T ${ELDF} ../csrc/feldspar-parallella.c $f -o ${ELF} -le-lib -lm -O2
     # Convert ebinaries to SREC files
     e-objcopy --srec-forceS3 --output-target srec ${ELF} ${SREC}
 done
